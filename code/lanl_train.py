8c5ccb8d1abc24058305794d98bea2a5d0657d21
Abhishek Ranjan <aranjan@nymi.com>
2019-05-20 11:38:34 -0400

    Added external memory feature loading

--- code/lanl_train.py
+++ code/lanl_train.py
@@ -11,11 +11,13 @@ import time
 # random seed for repeatability
 random.seed(0)
 np.random.seed(0)
-TRAINING_SAMPS = 5000
+TRAINING_SAMPS = 0
+GENERATE_LIBSVM = False #True
+LIBSVM_CACHE = './localdata/dtrain.cache'
 
 # set general training params
 train_params = {
-    'N_ROUND': 200, # number of learners
+    'N_ROUND': 100, # number of learners
 }
 
 jd = json.JSONDecoder()
@@ -23,11 +25,11 @@ settings = jd.decode(open('settings.json').read())
 
 # set xgb params
 xgb_params = {'nthread': settings['XGB_NTHREAD'],
-              'max_depth': 2,
+              'max_depth': 4,
               'eta': 0.01,
               'gamma': 0.1,
               'lambda': 0.1,
-              'min_child_weight': 1,              
+              'min_child_weight': 1,
               'objective': 'reg:linear',
               'eval_metric': 'mae',
               'subsample': 0.5,
@@ -35,34 +37,33 @@ xgb_params = {'nthread': settings['XGB_NTHREAD'],
               'silent': 0
               }
 
-###########
-# Training
-###########
-#def main(args):
-# if args.simple:
-#     # train a simplified classifier
-#     train_params['N_ROUND'] = 100
-#     xgb_params['eta'] = 0.1
-
 saved_model_file = './localdata/model-{}.npy'.format(dt.datetime.strftime(dt.datetime.now(), '%d%m%y%H%M%S'))
 
 print('-- Training using settings: {}'.format(settings))
 print("-- XGBoost params: {}".format(xgb_params))
 print("-- General training params: {}".format(train_params))
 
-# load precalculated training features
-ftrain = h5py.File(settings['TRAIN_FEATURES_X'], 'r')
-full_tr_count = ftrain[settings['DS_TRAIN_FEATURES']].shape[0]
+if GENERATE_LIBSVM:
+    # load precalculated training features
+    ftrain = h5py.File(settings['TRAIN_FEATURES_X'], 'r')
+    full_tr_count = ftrain[settings['DS_TRAIN_FEATURES']].shape[0]
 
-if TRAINING_SAMPS == 0:
-    TRAINING_SAMPS = full_tr_count
+    if TRAINING_SAMPS == 0:
+        TRAINING_SAMPS = full_tr_count
 
-allX = ftrain[settings['DS_TRAIN_FEATURES']][0:TRAINING_SAMPS,:,:].reshape(-1,feautils.FEA_DIM)
-allY = np.load(settings['TRAIN_FEATURES_Y'])[0:TRAINING_SAMPS, :]
+    allX = ftrain[settings['DS_TRAIN_FEATURES']][0:TRAINING_SAMPS,:,:].reshape(-1,feautils.FEA_DIM)
+    allY = np.load(settings['TRAIN_FEATURES_Y'])[0:TRAINING_SAMPS, :]
+    print('-- Generating LIBSVM training data for {} in {}'.format(allX.shape, settings['TRAINDATA_LIBSVM']))
+    import sklearn.datasets as skd
+    skd.dump_svmlight_file(allX, allY.ravel(), settings['TRAINDATA_LIBSVM'])
+    allX = []
+    allY = []
 
-dtrain = xgb.DMatrix(allX, allY)
+cache_train_data_path = settings['TRAINDATA_LIBSVM'] + '#' + LIBSVM_CACHE
+print('-- Loading cached data from {} '.format(cache_train_data_path))
+dtrain = xgb.DMatrix(cache_train_data_path)
+print('-- Loaded dtrain: {} x {}. Starting training...'.format(dtrain.num_row(), dtrain.num_col()))
 # training classifier
-print('-- Training starting with: X={}, Y={}'.format(allX.shape, allY.shape))
 starttime = time.time()
 #best_classifier = xgb.train(params=xgb_params, dtrain=dtrain, num_boost_round=train_params['N_ROUND'])
 best_classifier = xgb.cv(params=xgb_params, dtrain=dtrain, num_boost_round=train_params['N_ROUND'], nfold=3, seed=0)
@@ -72,9 +73,3 @@ elapsed_sec = endtime - starttime
 print('-- XGB training completed in {0:.2f} sec ({1:.2f} hrs)'.format(elapsed_sec, elapsed_sec/3600))
 np.save(saved_model_file, [best_classifier, xgb_params])
 print('-- Saved the full classifier to {0}'.format(saved_model_file))
-
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description="LANL XGB training")
-#     parser.add_argument('--simple', action='store_true',
-#                         help="Simplified model training for fast execution")
-#     main(parser.parse_args())    
