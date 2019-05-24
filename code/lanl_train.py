0eac79f17cea9b84df77f9f9877c0f45f6815352
Abhishek Ranjan <aranjan@nymi.com>
2019-05-24 09:56:19 -0400

    Latest stable pipeline for training/testing and gen submissin

--- code/lanl_train.py
+++ code/lanl_train.py
@@ -7,6 +7,7 @@ import random
 import json
 import argparse
 import time
+import logging
 
 # random seed for repeatability
 random.seed(0)
@@ -16,18 +17,22 @@ TRAINING_FEA_DIM = feautils.FEA_DIM
 GENERATE_LIBSVM = False
 USE_LIBSVM = True
 EVAL_FRAC = 0.2
+EVAL_STEP = 50
 
 # set general training params
 train_params = {
-    'N_ROUND': 100, # number of learners
+    'N_ROUND': 5000, # number of learners
 }
 
 jd = json.JSONDecoder()
 settings = jd.decode(open('settings.json').read())
 
+# configure logger
+logging.basicConfig(filename=settings['TMP_OUTPUT']+'run.log',level=logging.DEBUG, filemode='w')
+
 # set xgb params
 xgb_params = {'nthread': settings['XGB_NTHREAD'],
-              'max_depth': 4,
+              'max_depth': 6,
               'eta': 0.01,
               'gamma': 0.1,
               'lambda': 0.1,
@@ -39,11 +44,11 @@ xgb_params = {'nthread': settings['XGB_NTHREAD'],
               'silent': 0
               }
 
-saved_model_file = './localdata/model-{}.npy'.format(dt.datetime.strftime(dt.datetime.now(), '%d%m%y%H%M%S'))
+saved_model_file = settings['SUBMISSION_FOLDER'] + 'model-' + dt.datetime.strftime(dt.datetime.now(), '%d%m%y%H%M%S') + '.npy'
 
-print('-- Training using settings: {}'.format(settings))
-print("-- XGBoost params: {}".format(xgb_params))
-print("-- General training params: {}".format(train_params))
+logging.debug('-- Training using settings: {}'.format(settings))
+logging.debug("-- XGBoost params: {}".format(xgb_params))
+logging.debug("-- General training params: {}".format(train_params))
 
 ftrain = h5py.File(settings['TRAIN_FEATURES_X'], 'r')
 full_tr_count = ftrain[settings['DS_TRAIN_FEATURES']].shape[0]
@@ -57,12 +62,12 @@ train_ind_rand = np.sort(all_samp_ind[0:int((1-EVAL_FRAC)*TRAINING_SAMPS)])
 eval_ind_rand = np.sort(all_samp_ind[int((1-EVAL_FRAC)*TRAINING_SAMPS)+1:])
 
 if GENERATE_LIBSVM:
-    print('-- Generating LIBSVM training data: train index = {} ({}), eval index = {} ({})'.format(train_ind_rand.shape, train_ind_rand, eval_ind_rand.shape, eval_ind_rand))
+    logging.debug('-- Generating LIBSVM training data: train index = {} ({}), eval index = {} ({})'.format(train_ind_rand.shape, train_ind_rand, eval_ind_rand.shape, eval_ind_rand))
     allTrainX = ftrain[settings['DS_TRAIN_FEATURES']][train_ind_rand, 0:TRAINING_FEA_DIM,:].reshape(-1,TRAINING_FEA_DIM)
     allTrainY = np.load(settings['TRAIN_FEATURES_Y'])[train_ind_rand, :]
     allEvalX = ftrain[settings['DS_TRAIN_FEATURES']][eval_ind_rand, 0:TRAINING_FEA_DIM,:].reshape(-1,TRAINING_FEA_DIM)
     allEvalY = np.load(settings['TRAIN_FEATURES_Y'])[eval_ind_rand, :]
-    print('-- processing train = ({},{}), eval = ({},{})...'.format(allTrainX.shape, allTrainY.shape, allEvalX.shape, allEvalY.shape))
+    logging.debug('-- processing train = ({},{}), eval = ({},{})...'.format(allTrainX.shape, allTrainY.shape, allEvalX.shape, allEvalY.shape))
     import sklearn.datasets as skd
     starttime=time.time()
     skd.dump_svmlight_file(allTrainX, allTrainY.ravel(), settings['TRAINDATA_LIBSVM'])
@@ -72,14 +77,14 @@ if GENERATE_LIBSVM:
     allEvalX = []
     allEvalY = []
     endtime=time.time()
-    print('-- Dumped LIBSVM data file in {:.2f} min'.format((endtime-starttime)/60))
+    logging.debug('-- Dumped LIBSVM data file in {:.2f} min'.format((endtime-starttime)/60))
 
 if USE_LIBSVM:
     cache_train_data_path = settings['TRAINDATA_LIBSVM'] + '#' + settings['CACHE_TRAIN_LIBSVM']
-    print('-- Loading cached TRAIN data from {} '.format(cache_train_data_path))
+    logging.debug('-- Loading cached TRAIN data from {} '.format(cache_train_data_path))
     dtrain = xgb.DMatrix(cache_train_data_path)
     cache_eval_data_path = settings['EVALDATA_LIBSVM'] + '#' + settings['CACHE_EVAL_LIBSVM']
-    print('-- Loading cached EVAL data from {} '.format(cache_eval_data_path))
+    logging.debug('-- Loading cached EVAL data from {} '.format(cache_eval_data_path))
     deval = xgb.DMatrix(cache_eval_data_path)
 else:
     allTrainX = ftrain[settings['DS_TRAIN_FEATURES']][train_ind_rand, 0:TRAINING_FEA_DIM,:].reshape(-1,TRAINING_FEA_DIM)
@@ -89,17 +94,17 @@ else:
     dtrain = xgb.DMatrix(allTrainX, allTrainY)
     deval = xgb.DMatrix(allEvalX, allEvalY)
 
-print('-- dtrain loaded: {} x {}. Starting training...'.format(dtrain.num_row(), dtrain.num_col()))
-print('-- deval loaded: {} x {}. Starting training...'.format(deval.num_row(), deval.num_col()))
+logging.debug('-- dtrain loaded: {} x {}. Starting training...'.format(dtrain.num_row(), dtrain.num_col()))
+logging.debug('-- deval loaded: {} x {}. Starting training...'.format(deval.num_row(), deval.num_col()))
 # training classifier
 starttime = time.time()
 watchlist = [(deval,'eval'),(dtrain,'train')]
 evals_result = {}
-best_classifier = xgb.train(xgb_params, dtrain, train_params['N_ROUND'], watchlist, evals_result=evals_result)
+best_classifier = xgb.train(xgb_params, dtrain, train_params['N_ROUND'], watchlist, evals_result=evals_result, verbose_eval=EVAL_STEP)
 dtrain = None
 deval = None
 endtime = time.time()
 elapsed_sec = endtime - starttime
-print('-- XGB training completed in {0:.2f} sec ({1:.2f} hrs)'.format(elapsed_sec, elapsed_sec/3600))
-np.save(saved_model_file, [best_classifier, xgb_params, evals_result])
-print('-- Saved the full classifier and evals_result to {0}'.format(saved_model_file))
+logging.debug('-- XGB training completed in {0:.2f} sec ({1:.2f} hrs)'.format(elapsed_sec, elapsed_sec/3600))
+np.save(saved_model_file, [best_classifier, xgb_params, train_params, evals_result])
+logging.debug('-- Saved the full classifier and evals_result to {0}'.format(saved_model_file))
